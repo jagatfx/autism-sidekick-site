4066fa8be0cf903f8ed8c2a73fa9d851e2637f45
Jacob Madden <jacob.madden@gmail.com>
2019-02-18 01:36:40 -0500

    update

--- src/pages/index.js
+++ src/pages/index.js
@@ -39,42 +39,42 @@ class HomeIndex extends React.Component {
                                 <h3>Augmented Reality</h3>
                                 <p>See an enhanced world</p>
                             </header>
-                            <Link to="/landing" className="link primary"></Link>
+                            <Link to="/#" className="link primary"></Link>
                         </article>
                         <article style={{backgroundImage: `url(${pic02})`}}>
                             <header className="major">
                                 <h3>Emotional Buddy</h3>
                                 <p>Get help understanding emotions</p>
                             </header>
-                            <Link to="/landing" className="link primary"></Link>
+                            <Link to="/#" className="link primary"></Link>
                         </article>
                         <article style={{backgroundImage: `url(${anniePhoto})`}}>
                             <header className="major">
                                 <h3>Stimming Break</h3>
                                 <p>Built-in self-stimulation activities</p>
                             </header>
-                            <Link to="/landing" className="link primary"></Link>
+                            <Link to="/#" className="link primary"></Link>
                         </article>
                         <article style={{backgroundImage: `url(${workplacePhoto})`}}>
                             <header className="major">
                                 <h3>Scene Characterization</h3>
                                 <p>Understand what is occurring in scene</p>
                             </header>
-                            <Link to="/landing" className="link primary"></Link>
+                            <Link to="/#" className="link primary"></Link>
                         </article>
                         <article style={{backgroundImage: `url(${hidingPhoto})`}}>
                             <header className="major">
                                 <h3>Therapy Replay</h3>
                                 <p>Replay interactions with caregivers</p>
                             </header>
-                            <Link to="/landing" className="link primary"></Link>
+                            <Link to="/#" className="link primary"></Link>
                         </article>
                         <article style={{backgroundImage: `url(${wallPhoto})`}}>
                             <header className="major">
                                 <h3>Clarity</h3>
                                 <p>Focus the world to your frequency</p>
                             </header>
-                            <Link to="/landing" className="link primary"></Link>
+                            <Link to="/#" className="link primary"></Link>
                         </article>
                     </section>
                     <section id="two">
@@ -82,9 +82,9 @@ class HomeIndex extends React.Component {
                             <header className="major">
                                 <h2>Autism Sidekick</h2>
                             </header>
-                            <p>Autism Sidekick is a project focused on granting super powers to people living with ASD using augmented reality (AR). Powered by the Unity engine, the Autism Sidekick is an entry in the 2019 Unity for Humanity Challenge.</p>
+                            <p>Autism Sidekick is a project focused on granting super powers to people living with ASD using augmented reality (AR). Powered by the Unity engine, the Autism Sidekick is an entry in the <a href="https://connect.unity.com/challenges/unityforhumanity">2019 Unity for Humanity Challenge</a>.</p>
                             <ul className="actions">
-                                <li><Link to="/landing" className="button next">Learn More</Link></li>
+                                <li><Link to="https://s3.amazonaws.com/autism-sidekick/2019UnityforHumanity_AutismSidekick.pdf" className="button next">Learn More</Link></li>
                             </ul>
                         </div>
                     </section>
